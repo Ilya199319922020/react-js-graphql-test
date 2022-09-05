089665d828236fe7edee00807c5b950ba23f5a83
Ilya Smirnov <iliasmirnov93@mail.ru>
2022-09-05 23:55:10 +0300

    addd styles list element

--- src/components/PageList/PageElement/PageElement.js
+++ src/components/PageList/PageElement/PageElement.js
@@ -1,27 +1,47 @@
 import React from 'react';
+import style from './PageElement.module.css';
 
-const PageElement = ({elementLaunch}) => {
-	
+const PageElement = ({ elementLaunch }) => {
 	return (
-		<div>
-			<div>
-				<h6>name</h6>
-				<button>
+		<div
+			className={style.page}
+		>
+			<div
+				className={style.page__header}
+			>
+				<h6
+					className={style.page__header_item}
+				>
+					{
+						elementLaunch.mission_name
+					}
+				</h6>
+				<button
+					className={style.page__header_btn}
+				>
 					-&gt;
 				</button>
 			</div>
-			<div>
-				<div>
-					<span>
+			<div
+				className={style.page__info}
+			>
+				<div
+					className={style.page__info_type}
+				>
+					<span
+
+					>
 						Тип
 					</span>
 					<span>
-					{
-					  elementLaunch.mission_name
-					}
+						{
+							elementLaunch.mission_name
+						}
 					</span>
 				</div>
-				<div>
+				<div
+					className={style.page__info_location}
+				>
 					<span>
 						Порт
 					</span>
@@ -29,9 +49,6 @@ const PageElement = ({elementLaunch}) => {
 						{elementLaunch.rocket.rocket_name}
 					</span>
 				</div>
-			</div>
-			<div>
-				линия
 			</div>
 		</div>
 	);
--- src/components/PageList/PageList.js
+++ src/components/PageList/PageList.js
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
 import { GET_ELEMENT } from '../../graphql/query';
 import PageElement from './PageElement/PageElement';
+import styles from './PageList.module.css';
 
 const PageList = () => {
 	const [page, setPage] = useState(0);
@@ -28,29 +29,47 @@ const PageList = () => {
 	);
 
 	return (
-		<div>
-			<h4>SpaceX Ships</h4>
-			<div>
+		<div
+			className={styles.container}
+		>
+			<h4
+				className={styles.container__header}
+			>
+				SpaceX Ships
+			</h4>
+			<div
+				className={styles.container__list}
+			>
 				{
 					elements
 						? listLaunch
 						: <span>Loading...</span>
 				}
 			</div>
-			<div>
+			<div
+				className={styles.container__btn}
+			>
 				<button
+					className={styles.container__btn_prev}
 					disabled={!page}
 					onClick={() => setPage(prev => prev - 1)}
 				>
-					&lt;
+					<div>
+						&lt;
+					</div>
 				</button>
-				<span>
+				<span
+					className={styles.container__btn_page}
+				>
 					{page + 1}
 				</span>
 				<button
+					className={styles.container__btn_next}
 					onClick={() => setPage(prev => prev + 1)}
 				>
-					&gt;
+					<div>
+						&gt;
+					</div>
 				</button>
 			</div>
 		</div>
